0845c46bfe8401d2d55b7216cc15e6ff5a46b558
Daniel Steigerwald <daniel@steigerwald.cz>
2024-06-13 22:37:04 +0200

    Empty Next.js web

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Deconstruction of the State",
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Deconstruction of the State</h1>
+      <p>Soon.</p>
+    </>
+  );
+}
