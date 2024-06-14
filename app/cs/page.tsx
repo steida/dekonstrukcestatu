eccfe502f2fa43b94f5063301d105716427fd9b3
Daniel Steigerwald <daniel@steigerwald.cz>
2024-06-14 09:24:08 +0200

    Add intro and links

--- app/cs/page.tsx
+++ app/cs/page.tsx
@@ -9,7 +9,31 @@ export default function Page() {
     <>
       <h1>Dekonstrukce státu</h1>
       <Link href="/">English</Link>
-      <p>Soon.</p>
+      <p>
+        Máme problém. Ten problém je složitost státu. Stát se stal tak složitým,
+        že je nemožné jej řídit prostřednictvím volebního procesu. Jeden hlas
+        nemůže rozhodnout o téměř nekonečném množství otázek týkajících se
+        státu.
+      </p>
+      <p>
+        Řešením je dekonstrukce státu do menších částí, z nichž každá se
+        transformuje na akciovou společnost, od které každý občan dostane jednu
+        akcii, aby mohl vykonávat svá akcionářská práva.
+      </p>
+      <a href="https://x.com/steidacz" target="_blank">
+        x.com/steidacz
+      </a>
+      <h2>Slides</h2>
+      <ul>
+        <li>
+          <a
+            target="blank"
+            href="https://docs.google.com/presentation/d/1jMV7FbzONYyuTHvQZLJmxGwYf9_at2QK3XqRL7sV8v0"
+          >
+            Bitcoin - the endgame (PragueBTC 2024)
+          </a>
+        </li>
+      </ul>
     </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,30 @@ export default function Page() {
     <>
       <h1>Deconstruction of the State</h1>
       <Link href="/cs">Česky</Link>
-      <p>Soon.</p>
+      <p>
+        We have a problem. The problem is the State's complexity. The State is
+        so complex that it's impossible to manage through the election process.
+        One vote can't decide almost infinite issues related to the State.
+      </p>
+      <p>
+        The solution is to deconstruct the State into smaller parts, each of
+        which is transformed into a joint-stock company, and each citizen is
+        given one share to exercise their shareholder rights.
+      </p>
+      <a href="https://x.com/steida" target="_blank">
+        x.com/steida
+      </a>
+      <h2>Slides</h2>
+      <ul>
+        <li>
+          <a
+            target="blank"
+            href="https://docs.google.com/presentation/d/1jMV7FbzONYyuTHvQZLJmxGwYf9_at2QK3XqRL7sV8v0"
+          >
+            Bitcoin - the endgame (PragueBTC 2024)
+          </a>
+        </li>
+      </ul>
     </>
   );
 }
